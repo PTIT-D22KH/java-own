7ef3ddbcfffcc64c17eefe4da021a4e5c3a86e35
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-09 14:47:44 +0000

    add files

--- src/basic/Understading-variables/src/string_test1/Sample.java
+++ src/basic/Understading-variables/src/string_test1/Sample.java
@@ -3,6 +3,15 @@ public class Sample {
     public static void main(String[] args) {
         String var1 = "Study";
         String var2 = "Easy";
-        System.out.println(var1 + var2);
+        int var3 = 10;
+        String var4 = "100";
+        String var5 = "10";
+        String var6 = "Duongvct \u00f1";
+        System.out.println(var6);
+        System.out.println(var1 + var2 + var3);
+        System.out.println(Integer.parseInt(var4) + 10);
+        System.out.println(var4 + var5);
+        System.out.println(var3 + var4);
+        System.out.println(Integer.parseInt(var4) + var5);
     }
 }
---
+++ src/basic/Understading-variables/src/type_casting1/Sample.java
@@ -0,0 +1,9 @@
+package type_casting1;
+
+public class Sample {
+    public static void main(String[] args) {
+        short a1 = 10;
+        // byte a2 = a1;//cannot casting smaller to larger
+        int a2 = a1;
+    }
+}
