9db099bfd0d101dc89aa4c05fe403e888cbc78a0
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-09 14:18:33 +0000

    add files

---
+++ src/basic/Understading-variables/src/basic/HelloWorld.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int value1 = 10, value2;
+        value2 = 100 * 20;
+
+        // int value3 = 99999999999999999999999999;
+        System.out.println(value1 + value2);
+    }
+}
---
+++ src/basic/Understading-variables/src/basic/Sample.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Sample {
+    int max = 2147483647; // 4byte
+    int min = -2147483647;
+    short shortMax = 32467; //2byte
+    short shortMin = -32768;
+    long longMax = 9223372036854775807L;//8byte
+    long longMin = -9223372036854775808L;
+    byte byteMax = 127; //1byte
+    byte byteMin = -128;
+}
