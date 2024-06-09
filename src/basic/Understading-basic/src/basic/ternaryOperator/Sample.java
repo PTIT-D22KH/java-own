761317dd625d7da6265431ac3c4d64aea6267125
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-09 15:14:55 +0000

    add files

---
+++ src/basic/Understading-basic/src/basic/ternaryOperator/Sample.java
@@ -0,0 +1,9 @@
+package basic.ternaryOperator;
+
+public class Sample {
+    public static void main(String[] args) {
+        int x = 11;
+        String result = (x == 10) ? "X is 10" : "X is not 10";
+        System.out.println(result);
+    }
+}
