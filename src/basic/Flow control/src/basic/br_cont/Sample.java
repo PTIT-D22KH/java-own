4113e9113d8782ffe9550747fb9f0d9b184b0471
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-10 13:34:00 +0000

    add loops

---
+++ src/basic/Flow control/src/basic/br_cont/Sample.java
@@ -0,0 +1,14 @@
+package basic.br_cont;
+
+public class Sample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Before continue: ");
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+    }
+}
--- src/basic/Flow control/src/basic/loop1/Sample.java
+++ src/basic/Flow control/src/basic/loop1/Sample.java
@@ -2,8 +2,19 @@ package basic.loop1;
 
 public class Sample {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
+        int i = 5;
+        for (i = 0; i <= 10; ++i) {
             System.out.println(i);
         }
+        int x = 0;
+        while (x < 10) {
+            System.out.println("Iteration: " + x);
+            x++;
+        }
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        }while(j > 10);
     }
 }
