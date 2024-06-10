9f245984600fa60774c0b29655fe4fab2b5bea42
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-10 16:19:16 +0000

    add inheritance

--- src/basic/OOP/src/basic/inheritance1/Main.java
+++ src/basic/OOP/src/basic/inheritance1/Main.java
@@ -2,6 +2,10 @@ package basic.inheritance1;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Bike bike = new Bike("Petrol", 2, 2, 21, "LED", "Short");
+
+        // bike.handle = "Short";
+        // bike.engine = "Petrol";
+        System.out.println(bike.run());
     }
 }
