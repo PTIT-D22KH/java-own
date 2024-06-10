6e7dec1d3ca875a4ad9c7fde88d8523d714a1703
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-10 15:27:36 +0000

    add oop

---
+++ src/basic/OOP/src/basic/default_constructor/Main.java
@@ -0,0 +1,8 @@
+package basic.default_constructor;
+
+public class Main {
+    public static void main(String[] args) {
+        Smartphone phone = new Smartphone("Samsung");
+        System.out.println(phone.getBrand());
+    }
+}
---
+++ src/basic/OOP/src/basic/default_constructor/Smartphone.java
@@ -0,0 +1,19 @@
+package basic.default_constructor;
+
+public class Smartphone {
+    private String  brand;
+
+    
+
+    public Smartphone(String brand) {
+        this.brand = brand;
+    }
+
+
+
+    public String getBrand() {
+        return brand;
+    }
+    
+
+}
