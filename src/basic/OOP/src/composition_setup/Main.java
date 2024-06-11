a5de33f026d84c6ec95dc7c9a29f4ca092f4a3e4
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-11 02:57:26 +0000

    edit files

--- src/basic/OOP/src/composition_setup/Main.java
+++ src/basic/OOP/src/composition_setup/Main.java
@@ -1,7 +1,8 @@
 package composition_setup;
 
 import composition_setup.laptop.Laptop;
-// import composition_setup.laptop.components.Processor;
+import composition_setup.laptop.components.GraphicsCard;
+import composition_setup.laptop.components.Processor;
 
 public class Main {
 
@@ -10,6 +11,11 @@ public class Main {
 		System.out.println(laptop.getGraphicsCard().getBrand());
 
 
+		Processor p1 = new Processor();
+		GraphicsCard g1 = new GraphicsCard();
+
+		Laptop l1  = new Laptop(24.0f,p1 , "DDR5", "1TB", g1,  "Single Layer", "backLed");
+		System.out.println(l1);
 	}
 
 }
