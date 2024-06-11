8368c9d5477e5b7f92b95ba8c1220a6264a77d58
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-11 02:24:38 +0000

    add files

--- src/basic/OOP/src/Main.java
+++ src/basic/OOP/src/Main.java
@@ -6,5 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Reptile reptile = new Reptile();
         System.out.println(reptile.showInfo());
+        Fish fish = new Fish();
+        System.out.println(fish.showInfo());
+        Animal animal = new Animal();
+        System.out.println(animal.showInfo());
+        Bird bird = new Bird();
+        System.out.println(bird.showInfo());
     }
 }
