3c472e71f18dc8442a9b96d81329abc1c0464d73
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-11 02:49:47 +0000

    edit files

--- src/basic/OOP/src/composition_setup/Main.java
+++ src/basic/OOP/src/composition_setup/Main.java
@@ -1,13 +1,13 @@
 package composition_setup;
 
 import composition_setup.laptop.Laptop;
-import composition_setup.laptop.components.Processor;
+// import composition_setup.laptop.components.Processor;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Laptop laptop = new Laptop();
-		System.out.println(laptop.getProcessor().getBrand());
+		System.out.println(laptop.getGraphicsCard().getBrand());
 
 
 	}
