1f0146cbc0283da5c15b38c54075ddc65b3ed7ec
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-11 03:40:01 +0000

    add polymorphism

---
+++ src/basic/OOP/src/basic/polymorphism1/Nokia3310.java
@@ -0,0 +1,8 @@
+package basic.polymorphism1;
+
+public class Nokia3310 extends Phone{
+    // @Override
+    public void feature() {
+        System.out.println("make calls and super reliable");
+    }
+}
---
+++ src/basic/OOP/src/basic/polymorphism1/Phone.java
@@ -0,0 +1,7 @@
+package basic.polymorphism1;
+
+public class Phone {
+    public void  feature() {
+        System.out.println("make calls");
+    }
+}
