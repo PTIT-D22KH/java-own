dbe28e731336ff93f857d6261660282bef91f178
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-11 03:46:39 +0000

    add polymorphism

--- src/basic/OOP/src/inheritance_ex/Main.java
+++ src/basic/OOP/src/inheritance_ex/Main.java
@@ -1,17 +1,43 @@
 package inheritance_ex;
+
+
 import inheritance_ex.animal.Animal;
 import inheritance_ex.animal.bird.*;
 import inheritance_ex.animal.fish.*;
 import inheritance_ex.animal.reptile.*;
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
-        Reptile reptile = new Reptile();
-        System.out.println(reptile.showInfo());
-        Fish fish = new Fish();
-        System.out.println(fish.showInfo());
+        
+        // Reptile reptile = new Reptile();
+        // System.out.println(reptile.showInfo());
+        // Fish fish = new Fish();
+        // System.out.println(fish.showInfo());
+        // Animal animal = new Animal();
+        // System.out.println(animal.showInfo());
+        // Bird bird = new Bird();
+        // System.out.println(bird.showInfo());
+        List<Animal> animals = new ArrayList<>();
         Animal animal = new Animal();
-        System.out.println(animal.showInfo());
-        Bird bird = new Bird();
-        System.out.println(bird.showInfo());
+        Reptile reptile = new Reptile();
+        Animal croc = new Crocodile();
+        Animal eel = new Eel();
+        Bird eagle = new Eagle();
+
+        animals.add(animal);
+        animals.add(reptile);
+        animals.add(croc);
+        animals.add(eel);
+        animals.add(eagle);
+
+        listAnimals(animals);
+
+       
+    }
+    private static void listAnimals(List<Animal> animals) {
+        for (Animal animal : animals) {
+            System.out.println(animal.showInfo());
+        }
     }
 }
