a7f2e3c44a11c543c6770839579db1f8693f4d69
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-16 12:31:27 +0000

    add files

--- src/basic/java_core/src/interface_java/interface_java/Main.java
+++ src/basic/java_core/src/interface_java/interface_java/Main.java
@@ -4,11 +4,13 @@ public class Main {
         SamsungPhone phone1 = new SamsungPhone();
         String p = phone1.processor();
         System.out.println(p);
+        System.out.println(phone1.spaceInGb());
         
         Iphone phone2 = new Iphone();
         String s;
         s = phone2.processor();
         System.out.println(s);
+        System.out.println(phone2.spaceInGb());
     }
     
 
