22da726f859f866dff0d4bcd78fad7ab37f7917e
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-16 12:15:32 +0000

    add files

---
+++ src/basic/java_core/src/interface_java/interface_java/Iphone.java
@@ -0,0 +1,6 @@
+package interface_java;
+public class Iphone {
+    public String processor() {
+        return "A15";
+    }
+}
--- src/basic/java_core/src/interface_java/interface_java/Main.java
+++ src/basic/java_core/src/interface_java/interface_java/Main.java
@@ -1,4 +1,17 @@
 package interface_java;
 public class Main {
-    SamsungPhone phone1 = new SamsungPhone();
+    public static void main(String[] args) {
+        SamsungPhone phone1 = new SamsungPhone();
+        int p = phone1.processor();
+        System.out.println(p);
+        
+        Iphone phone2 = new Iphone();
+        String s;
+        s = phone2.processor();
+        System.out.println(s);
+    }
+    
+
+
+
 }
