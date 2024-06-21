9daf03c396fc4958cda37fb73ba72d373462d18d
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-21 05:17:55 +0000

    add files

---
+++ src/basic/java_core/src/static_inner_class/Outer.java
@@ -0,0 +1,19 @@
+package static_inner_class;
+
+
+public class Outer {
+
+    public static class Inner {
+        public static int x = 10;
+        public static void staticMessage() {
+            System.out.println("Hello static");
+        }
+    }
+
+    public class NonStaticInner{
+        public void nonStaticMessage() {
+            System.out.println("Hello non static");
+        }
+    }
+
+}
