21033700246b5e2797583b8202af9618863d3ecb
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-21 02:28:30 +0000

    Pending changes exported from your codespace

--- src/basic/java_core/src/abstract_class/Main.java
+++ src/basic/java_core/src/abstract_class/Main.java
@@ -1,3 +1,22 @@
-public class Main {
+package basic.java_core.src.abstract_class;
 
+public class Main {
+    public static void main(String[] args) {
+        Person john = new John();
+        john.eat();
+        john.speak();
+
+        Person person = new Pooja();
+        person.eat();
+        person.speak();
+
+        Person p2 = new Person() {
+            @Override
+            public void eat() {
+                System.out.println("Eat food");
+            }
+        };
+        p2.speak();
+        p2.eat();
+    }
 }
---
+++ src/basic/java_core/src/abstract_class/Pooja.java
@@ -0,0 +1,8 @@
+package basic.java_core.src.abstract_class;
+
+public class Pooja extends Person{
+    @Override
+    public void eat() {
+        System.out.println("Pooja eats non-veg food");
+    }
+}
