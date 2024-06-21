fa1139a8777804d3c0639a2ff6675f42571e7c34
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-21 04:02:29 +0000

    add files

--- src/basic/Section 07 - Core concepts/S07L09 - Anonymous object/src/main/java/org/studyeasy/Main.java
+++ src/basic/Section 07 - Core concepts/S07L09 - Anonymous object/src/main/java/org/studyeasy/Main.java
@@ -3,14 +3,19 @@ package org.studyeasy;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(args[1]);
+        // System.out.println(args[1]);
 
-        if (args[0].equals(new Lock().getCode())){
+        // if (args[0].equals(new Lock().getCode())){
+        //     System.out.println("The door is now open");
+        // }else{
+        //     System.out.println("The door is closed");
+        // }
+
+        Lock lock  = new Lock();
+        if (lock.getCode() == "123456") {
             System.out.println("The door is now open");
-        }else{
-            System.out.println("The door is closed");
+        } else {
+            System.out.println("The door is now closed");
         }
-
-
     }
 }
