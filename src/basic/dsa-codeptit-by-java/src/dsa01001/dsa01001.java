f0c0427de8000b126144763d71b10069b0b323fb
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-22 08:27:47 +0000

    add files

--- src/basic/dsa-codeptit-by-java/src/dsa01001/dsa01001.java
+++ src/basic/dsa-codeptit-by-java/src/dsa01001/dsa01001.java
@@ -6,12 +6,30 @@ public class dsa01001 {
     public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         int t = input.nextInt();
-        while (t-- > 0) {
+        // System.out.println(t);
+        while (t > 0) {
             testCase();
+            t--;
         }
     }
     public static void testCase() {
         String s = input.next();
-        System.out.println(a);
+        char []c = s.toCharArray();
+        int n = s.length();
+        int i = n- 1;
+        while (i >= 0 && c[i] == '1') {
+            c[i] = '0';
+            i--;
+        }
+        if (i >= 0) {
+            c[i] = '1';
+            System.out.println(c);
+        } else {
+            for (int j = 1; j <= n; j++) {
+                System.out.printf("0");
+            }
+            System.out.println();
+        }
+
     }
 }
