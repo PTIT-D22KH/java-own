bf0a086c4c3b7d02c7d0b660eb3dfdb27be1bfa9
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-22 08:01:32 +0000

    add files

--- src/basic/dsa-codeptit-by-java/src/dsa01001/dsa01001.java
+++ src/basic/dsa-codeptit-by-java/src/dsa01001/dsa01001.java
@@ -3,8 +3,15 @@ package dsa01001;
 import java.util.*;
 
 public class dsa01001 {
-    public Scanner input = new Scanner(System.in);
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int t = input.nextInt();
+        while (t-- > 0) {
+            testCase();
+        }
+    }
+    public static void testCase() {
+        String s = input.next();
+        System.out.println(a);
     }
 }
