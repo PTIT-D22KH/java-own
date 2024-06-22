8c6f7986acb68286b4de66f8fc996ea1623ef174
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-22 07:46:50 +0000

    add files

---
+++ src/basic/dsa-codeptit-by-java/src/dsa01001/dsa01001.java
@@ -0,0 +1,10 @@
+package dsa01001;
+
+import java.util.*;
+
+public class dsa01001 {
+    public Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
