7b06be1927f7b2ec82a6343e9265dc158c0c4917
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-23 08:30:22 +0000

    add files

---
+++ src/basic/dsa-codeptit-by-java/src/dsa05001/dsa05001.java
@@ -0,0 +1,31 @@
+package dsa05001;
+import java.util.*;
+public class dsa05001 {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Write your code here
+        int t = input.nextInt();
+        while (t-- > 0) {
+            testCase();
+        }
+    }
+    public static void testCase( ){
+        
+        String s1 = input.next();
+        String s2 = input.next();
+        int n = s1.length();
+        int m = s2.length();
+        int [][]dp = new int[n + 1][m + 1];
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(dp[n][m]);
+    }
+}
