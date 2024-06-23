df2fbe8a2c43d32fc9d9c1ebcd0c7a1e904d3424
vctduong <165612382+vctduong@users.noreply.github.com>
2024-06-23 07:43:43 +0000

    add files

---
+++ src/basic/dsa-codeptit-by-java/src/dsa01005/dsa01005.java
@@ -0,0 +1,18 @@
+package dsa01005;
+
+import java.util.Scanner;
+
+public class dsa01005 {
+    public static Scanner input = new Scanner(System.in);
+    static int n;
+    public static void main(String[] args) {
+        // Write your code here
+        int t = input.nextInt();
+        while (t-- > 0) {
+            testCase();
+        }
+    }
+    public static void testCase() {
+        
+    }
+}
